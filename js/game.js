518e27b0b44622bc2eca78719ccc6fda57e2704a
DFD-M <seresha_2@Mail.ru>
2020-12-21 19:12:52 +0300

    Добавил физику столкновений, но не полностью

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@ var xPos= 15
 var yPos= 613
 var gravity=0.5
 var n=0
-var flag=0;
+
 
 
 
@@ -31,7 +31,7 @@ function gotimer(){
 
    
 }
-
+var ground = new Image();
 var character=new Image();
 var key = new Image();
 var bg = new Image();
@@ -44,12 +44,45 @@ var block2= new Image();
 character.src="photos/sprite (3).png"
 key.src="Photos/ket.png"
 bg.src="Photos/2_21.png"
-// enemy.src=""
+ground.src="photos/tileset.png"
 block1.src="Photos/tileset1.png"
 block2.src="Photos/tileset1.png"
 
 
 
+
+var isCollided = function(obst, obj) {
+    if (obj.x + obj.width > obst.x 
+    && obj.x < obst.x + obst.width
+    && obj.y < obst.y + obst.height
+    && obj.y + obj.height > obst.y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var collideHandler = function(obst, obj) {
+    if (isCollided(obst, obj)) {
+        if (obj.xPrev >= obst.x + obst.width) {
+            obj.x = obst.x + obst.width;
+            obj.xVelocity = 0;
+        }
+        if (obj.xPrev + obj.width <= obst.x) {
+            obj.x = obst.x - obj.width;
+            obj.xVelocity = 0;
+        }
+        if (obj.yPrev + obj.height <= obst.y) {
+            obj.y = obst.y - obj.height;
+            obj.yVelocity = 0;
+            obj.jumping = false;
+        }
+        if (obj.yPrev >= obst.y + obst.height) {
+            obj.y = obst.y + obst.height;
+            obj.yVelocity = 0;
+        }
+    }
+}
 
 
 
@@ -57,11 +90,12 @@ const player={
     
     x:15,
     y:613,
+    xPrev:0,
+    yPrev:0,
     width:33,
     height:32,
     frameX:0,
     frameY:0,
-    speed:9,
     size:10,
     jumping:false,
     x_velocity:0,
@@ -70,12 +104,14 @@ const player={
 
 
 const blocks={
-    x:0,
-    y:0,
-    width:0,
-    height:0,
+    x:150,
+    y:612,
+    width:45,
+    height:45,
     frameX:0,
     frameY:0,
+    x_velocity:0,
+    y_velocity:0,
     
 }
 
@@ -86,7 +122,9 @@ document.addEventListener('keydown',function (e){if(KeyDown(e));});
 
 
 function KeyDown(e)
-{
+{   player.xPrev=player.x;
+    player.yPrev=player.y;
+
     switch(e.keyCode){
         case 37: //left
             player.x_velocity-=15;
@@ -117,6 +155,8 @@ function KeyDown(e)
 
             break;
         
+            
+        
 
     }
     
@@ -128,8 +168,8 @@ function KeyDown(e)
 
 var block=[]
 block[0]={
-    x:cvs.width,
-    y:590
+    x:cvs.width/2,
+    y:cvs.height*0
 
 }
 
@@ -140,16 +180,33 @@ function draw(){
     ctx.drawImage(bg,0,0,canvas.width,canvas.height);
     ctx.drawImage(key,55,590,99,99)
     ctx.drawImage(character,player.width*player.frameX,player.height*player.frameY,player.width,player.height,player.x,player.y,player.width,player.height);
-    // animate();
-    // update();
     
+    // ctx.drawImage(ground,blocks.width*blocks.frameX,blocks.height*blocks.frameY,blocks.width,blocks.height,blocks.x,blocks.y,blocks.width,blocks.height);
+    // for(var i=0;i<2;i++){
+    //     blocks.x_velocity=0;
+        
+    //     ctx.drawImage(ground,blocks.width*blocks.frameX,blocks.height*blocks.frameY,blocks.width,blocks.height,blocks.x,blocks.y,blocks.width,blocks.height);
+          
+    //     if(blocks.x>400){
+            
+    //         // blocks.frameY=2;
+    //         // blocks.frameX=3;
+    //         ctx.drawImage(ground,blocks.width*blocks.frameX,blocks.height*blocks.frameY,blocks.width,blocks.height,blocks.x-15,blocks.y,blocks.width,blocks.height)
+            
+    //     } 
+    // }
     for(var i=0;i<block.length;i++){
         ctx.drawImage(block1,block[i].x,block[i].y)
-        block[i].x--;
-        if(block[i].x==700){
+        
+        // block[i].x-=0.5;
+
+        block[i].y+=2;
+       
+        if(block[i].y==canvas.height/2){
             block.push({
-                x:cvs.width,
-                y:Math.floor(Math.random()*block1.width)-block1.width
+                x:block[i].x*Math.random()-130,
+                // y:Math.floor(Math.random()*block1.width)//-block1.width
+                y:Math.floor(canvas.height*0)
             })
         
         }
@@ -157,17 +214,27 @@ function draw(){
       
     }
     
+
+    // if(player.x>=canvas.width/2){
+    //     blocks.frameY=2
+    // }
+
+    //Wall
     if (player.x < player.width/3|| player.x > 1280 - player.width/3) {
         player.x=200;
     }
    
       
   
+
+
+
     player.y_velocity+=1.5;//gravity
     player.x+=player.x_velocity;
     player.y+=player.y_velocity;
     player.x_velocity*=0.3;
     player.y_velocity*=0.9;
+
 
     if(player.y>613){
         player.jumping=false;
@@ -175,14 +242,13 @@ function draw(){
         player.y_velocity=0;
     }
 
-    // if(yPos>=613){
-    //     gravity=0
-    // }else{
-    //     gravity=2
-    // }
+   
+    blocks.x+=blocks.x_velocity;
+    blocks.y+=blocks.y_velocity;
+    blocks.x_velocity*=0.3;
+    blocks.y_velocity*=0.9;
 
    
-    yPos+=gravity;
     ctx.font="20px Vernada"
     // ctx.fillText("Таймер:"+ ,0,100);
     ctx.fillText("Счет "+n,0,120)
